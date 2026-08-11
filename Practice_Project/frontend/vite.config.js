30517ea91fed54874439096512041058fd5ce6f9
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-08-11 08:18:43 +0545

    CI-CD Pipeline - Front End

--- Practice_Project/frontend/vite.config.js
+++ Practice_Project/frontend/vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-  },
   // Use relative base path or your exact repository name: '/<repository-name>/'
   base: './', 
-  
+
 });
